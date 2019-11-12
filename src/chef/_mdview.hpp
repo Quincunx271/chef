c325917b57f63ab5ca75b0a4a51d50f57c9f9f2d
Justin Bassett <jbassett271@gmail.com>
2019-11-11 17:58:59 -0800

    Add _mdview
    A multidimensional array-view with row-major access.

---
+++ src/chef/_mdview.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <numeric>
+#include <type_traits>
+
+namespace chef {
+    // row-major multidimensional array view
+    template <typename Container, int DimensionCount>
+    class _mdview
+    {
+    public:
+        using underlying_type = Container;
+        static constexpr auto num_dimensions = DimensionCount;
+
+        template <typename... Dims>
+        explicit constexpr _mdview(Container& container,
+            Dims const... dims) noexcept //
+            requires(sizeof...(Dims) == num_dimensions
+                && (std::convertible_to<std::size_t, Dims const> && ...))
+            : _mdview(container, {std::size_t(dims)...})
+        {}
+
+        explicit constexpr _mdview(
+            Container& container, std::array<std::size_t, num_dimensions> const dimensions) noexcept
+            : container_(&container)
+            , dimensions_(dimensions)
+        {
+            // if constexpr (std::ranges::sized_range<Container>) {
+            //      assert(std::accumulate(dimensions_.begin(), dimensions_.end(), 1,
+            //          std::multiplies()) == std::ranges::size(container));
+            // }
+        }
+
+        constexpr auto dimensions() const noexcept -> std::array<std::size_t, num_dimensions>
+        {
+            return dimensions_;
+        }
+
+        constexpr auto container() const -> Container*
+        {
+            return container_;
+        }
+
+        constexpr auto operator[](std::array<std::size_t, num_dimensions> const indices) const
+            -> decltype(auto)
+        {
+            assert(in_bounds(indices));
+            return (*container_)[compute_index(indices)];
+        }
+
+        template <typename... Dims>
+        constexpr auto at(Dims const... indices) const noexcept -> std::size_t
+            requires(sizeof...(Dims) == num_dimensions)
+        {
+            return at({std::size_t(indices)...});
+        }
+
+        constexpr auto at(std::array<std::size_t, num_dimensions> const indices) const
+            -> decltype(auto)
+        {
+            if (!in_bounds(indices)) throw std::out_of_range("at least one index is out of range");
+            return (*this)[indices];
+        }
+
+        template <typename... Dims>
+        constexpr auto compute_index(Dims const... indices) const noexcept -> std::size_t
+            requires(sizeof...(Dims) == num_dimensions)
+        {
+            return compute_index({std::size_t(indices)...});
+        }
+
+        constexpr auto compute_index(std::array<std::size_t, num_dimensions> const indices) const
+            noexcept -> std::size_t
+        {
+            using std::begin;
+            using std::cbegin;
+            using std::cend;
+
+            assert(in_bounds(indices));
+
+            std::array<std::size_t, num_dimensions> acc_dimensions;
+            acc_dimensions[0] = 1;
+
+            std::partial_sum(cbegin(dimensions_), cend(dimensions_) - 1, begin(acc_dimensions) + 1,
+                std::multiplies());
+
+            auto const index = std::transform_reduce(cbegin(acc_dimensions), cend(acc_dimensions),
+                cbegin(indices), std::size_t(0), std::plus(), std::multiplies());
+
+            assert(index < compute_size(*container_));
+
+            return index;
+        }
+
+    private:
+        Container* container_;
+        std::array<std::size_t, num_dimensions> dimensions_;
+
+        constexpr bool in_bounds(std::array<std::size_t, num_dimensions> indices) const
+        {
+            using std::cbegin;
+            using std::cend;
+
+            // Every index is less than the corresponding dimension size.
+            return std::transform_reduce(cbegin(indices), cend(indices), cbegin(dimensions_), true,
+                std::logical_and(), std::less());
+        }
+
+        static constexpr auto compute_size(Container const& container) -> std::size_t
+            requires(requires {
+                // clang-format off
+                { std::size(container) } -> std::convertible_to<std::size_t>;
+                // clang-format on
+            })
+        {
+            return size(container);
+        }
+
+        static constexpr auto compute_size(Container const&) -> std::size_t
+        {
+            // max size:
+            return std::size_t(-1);
+        }
+    };
+
+    template <int Dims>
+    constexpr auto _make_mdview = [](auto& container, auto const... dims) {
+        return _mdview<std::remove_reference_t<decltype(container)>, Dims>(container, dims...);
+    };
+}
---
+++ src/chef/_mdview.test.cpp
@@ -0,0 +1,101 @@
+#include <chef/_mdview.hpp>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("_mdview computes index correctly")
+{
+    auto const vec = std::vector<int>{
+        // clang-format off
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        // clang-format on
+    };
+
+    auto const mdview = chef::_make_mdview<2>(vec, 4, 2);
+
+    CHECK(mdview.compute_index(0, 0) == 0);
+    CHECK(mdview.compute_index(1, 0) == 1);
+    CHECK(mdview.compute_index(2, 0) == 2);
+    CHECK(mdview.compute_index(3, 0) == 3);
+    CHECK(mdview.compute_index(0, 1) == 4);
+    CHECK(mdview.compute_index(1, 1) == 5);
+    CHECK(mdview.compute_index(2, 1) == 6);
+    CHECK(mdview.compute_index(3, 1) == 7);
+}
+
+TEST_CASE("_mdview accesses correctly")
+{
+    auto vec = std::vector<int>{
+        // clang-format off
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        // clang-format on
+    };
+
+    auto const mdview = chef::_make_mdview<2>(vec, 4, 2);
+
+    CHECK(mdview.at(0, 0) == 1);
+    CHECK(mdview.at(1, 0) == 2);
+    CHECK(mdview.at(2, 0) == 3);
+    CHECK(mdview.at(3, 0) == 4);
+    CHECK(mdview.at(0, 1) == 5);
+    CHECK(mdview.at(1, 1) == 6);
+    CHECK(mdview.at(2, 1) == 7);
+    CHECK(mdview.at(3, 1) == 8);
+
+    CHECK(mdview[{0, 0}] == 1);
+    CHECK(mdview[{1, 0}] == 2);
+    CHECK(mdview[{2, 0}] == 3);
+    CHECK(mdview[{3, 0}] == 4);
+    CHECK(mdview[{0, 1}] == 5);
+    CHECK(mdview[{1, 1}] == 6);
+    CHECK(mdview[{2, 1}] == 7);
+    CHECK(mdview[{3, 1}] == 8);
+
+    mdview[{2, 1}] = 7;
+    CHECK(mdview[{2, 1}] == 7);
+    CHECK(vec.at(1 * 4 + 2) == 7);
+}
+
+TEST_CASE("_mdview works for 1 dimensional view")
+{
+    auto const vec = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7};
+
+    auto const mdview = chef::_make_mdview<1>(vec, vec.size());
+
+    CHECK(mdview.compute_index(0) == 0);
+    CHECK(mdview.compute_index(1) == 1);
+    CHECK(mdview.compute_index(2) == 2);
+    CHECK(mdview.compute_index(3) == 3);
+    CHECK(mdview.compute_index(4) == 4);
+    CHECK(mdview.compute_index(5) == 5);
+    CHECK(mdview.compute_index(6) == 6);
+    CHECK(mdview.compute_index(7) == 7);
+}
+
+TEST_CASE("_mdview works for 3 dimensional view")
+{
+    auto const vec = std::vector<int>{
+        // clang-format off
+        111, 112, 113, 114,
+        121, 122, 123, 124,
+
+        211, 212, 213, 214,
+        221, 222, 223, 224,
+
+        311, 312, 313, 314,
+        321, 322, 323, 324,
+        // clang-format on
+    };
+
+    auto const mdview = chef::_make_mdview<3>(vec, 4, 2, 3);
+
+    for (std::size_t z = 1; z <= 3; ++z) {
+        for (std::size_t y = 1; y <= 2; ++y) {
+            for (std::size_t x = 1; x <= 4; ++x) {
+                CAPTURE(x, y, z);
+                CHECK(mdview[{x - 1, y - 1, z - 1}] == z * 100 + y * 10 + x);
+            }
+        }
+    }
+}
