ba05887e55d9074257bdeac75c3d816ae37703e3
Justin Bassett <jbassett271@gmail.com>
2021-06-12 21:29:02 -0700

    Implement hopcroft's algorithm

--- src/chef/_/set.hpp
+++ src/chef/_/set.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <unordered_set>
 
 #include <tl/tl.hpp>
@@ -17,11 +18,25 @@ namespace chef::detail {
 	}
 
 	template <typename T, typename... Rest>
+	bool has_intersect(
+		std::unordered_set<T, Rest...> const& lhs, std::unordered_set<T, Rest...> const& rhs)
+	{
+		return std::any_of(lhs.begin(), lhs.end(), [&](auto const& x) { return rhs.contains(x); });
+	}
+
+	template <typename T, typename... Rest>
 	std::unordered_set<T, Rest...> difference(
 		std::unordered_set<T, Rest...> const& lhs, std::unordered_set<T, Rest...> const& rhs)
 	{
 		namespace views = std::ranges::views;
 		return chef::detail::to_container<std::unordered_set<T, Rest...>>(
 			lhs | views::filter([&](auto const& x) { return !rhs.contains(x); }));
+	}
+
+	template <typename T, typename... Rest>
+	bool has_difference(
+		std::unordered_set<T, Rest...> const& lhs, std::unordered_set<T, Rest...> const& rhs)
+	{
+		return std::any_of(lhs.begin(), lhs.end(), [&](auto const& x) { return !rhs.contains(x); });
 	}
 }
