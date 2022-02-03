2479059bed6e14bb347fa517e319d58a7ef368a9
Justin Bassett <jbassett@google.com>
2022-02-02 21:58:08 -0800

    Add the basic idea of the AST types

---
+++ src/chef/cfg/ast.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+
+// Defines a runtime AST for chef.
+//
+// It remains to be decided whether this will include concrete pieces such as
+// punctuation, or whether it will only include the abstract information.
+
+namespace chef::rt_ast {
+	struct ast_node;
+
+	struct sequence_node {
+		std::vector<std::unique_ptr<ast_node>> children;
+	};
+
+	struct ast_node {
+		using value_type = std::variant<sequence_node>;
+
+		// The name of the rule that produced this node.
+		std::string name;
+		// For nodes which are not the full node of the rule, this identifies
+		// which piece of the larger rule the node comes from.
+		std::size_t id = 0;
+
+		value_type value;
+	};
+}
